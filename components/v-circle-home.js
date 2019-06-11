b4e6bac43bf3b751e846e3b0cb811f49dc676e95
Naouel A <jchabert0@gmail.com>
2019-06-11 13:41:46 +0200

    circle random scale anim

--- components/v-circle-home.js
+++ components/v-circle-home.js
@@ -1,10 +1,15 @@
+
+var time = (Math.floor(Math.random() * 5) + 2); // beetween 2 and 6
+
 Vue.component('v-circle-home', {
     props: ['circle'],
     template: '<div class="circle" \
-                    style="border-radius: 100%; \
-                    position: absolute; \
-                    background-color: #3287f5; \
-                    opacity: .5" \
-                    :style="circle"> \
-                </div>'
+        style="border-radius: 100%; \
+        animation: scale '+time+'s ease-out 0s alternate infinite \
+        position: absolute; \
+        background-color: #3287f5; \
+        opacity: .5" \
+        :style="circle"> \
+    </div>'
 })
+            
