551a51d72c0e850644a06a6a4a531e0ed60e0f7c
Naouel A <jchabert0@gmail.com>
2019-06-11 14:19:29 +0200

    circle ok

--- components/v-circle-home.js
+++ components/v-circle-home.js
@@ -1,11 +1,9 @@
 
-var time = (Math.floor(Math.random() * 5) + 2); // beetween 2 and 6
-
 Vue.component('v-circle-home', {
     props: ['circle'],
     template: '<div class="circle" \
         style="border-radius: 100%; \
-        animation: scale '+time+'s ease-out 0s alternate infinite \
+        animation: scale ease-out 0s alternate infinite; \
         position: absolute; \
         background-color: #3287f5; \
         opacity: .5" \
