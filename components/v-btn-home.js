4cf20c8f4603e930c336b6ea0b3200415c775773
Naouel A <jchabert0@gmail.com>
2019-06-13 00:11:46 +0200

    v-btn rename v-btn-home

--- components/v-btn-home.js
+++ components/v-btn-home.js
@@ -1,4 +1,4 @@
-Vue.component('v-btn', {
+Vue.component('v-btn-home', {
     props: ['title', 'url', 'style', 'icon'],
     template: '<a class="button is-outlined is-rounded" style="width: 200px" \
                     :style="style" \
