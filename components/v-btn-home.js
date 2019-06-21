29d54f607b1d736c6e9e9365e82e6fa8b190c79f
jchabert0 <jchabert0@gmail.com>
2019-06-21 13:04:09 +0200

    btn fonction ok

--- components/v-btn-home.js
+++ components/v-btn-home.js
@@ -1,6 +1,6 @@
 Vue.component('v-btn-home', {
     props: ['title', 'url', 'style', 'icon'],
-    template: '<a class="button is-outlined is-rounded" style="width: 150px" \
+    template: '<a class="button is-outlined is-rounded" style="width: 200px" \
                     :style="style" \
                     :href="url"> \
                     <span class="icon is-small"> \
