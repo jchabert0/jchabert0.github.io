b50f1b7f63fc199a9e21705ea7835ea6df006e11
jchabert0 <jchabert0@gmail.com>
2019-06-21 13:45:36 +0200

    one component for contact links

--- components/v-btn-contact.js
+++ components/v-btn-contact.js
@@ -3,7 +3,7 @@
 Vue.component('v-btn-contact', {
     props: ['title', 'url', 'icon'],
     template: '<span class="icon" style="color: hsl(328,100%,54%); font-size: large" target="_blank"> \
-                <i :class="icon" style="padding: 1rem;"></i> \
+                <i :class="icon" style="padding:5px;"></i> \
                 {{ title }} \
                 </span>'
 })
--- components/v-btn-follow.js
+++ components/v-btn-follow.js
@@ -1,8 +1,8 @@
 
 Vue.component('v-btn-follow', {
-    props: ['url', 'icon'],
+    props: ['title', 'url', 'icon'],
     template: '<a style="color: hsl(328,100%,54%); font-size: -webkit-xxx-large; padding: 1rem;" target="_blank"\
                 :href="url"> \
                 <i :class="icon"></i> \
-                </a>'
+                </a><span> {{title}} </span>'
 })
